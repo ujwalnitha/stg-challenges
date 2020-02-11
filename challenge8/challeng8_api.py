2d8910d5ec8f9fc95dea819a4824d88b1f089d40
Nitha Ujwal <nitha.ujwal@dish.com>
2020-02-11 13:16:14 -0700

    Challenge 8 for multiple search tags

--- challenge8/challeng8_api.py
+++ challenge8/challeng8_api.py
@@ -13,7 +13,9 @@ from termcolor import colored
 class Challenge8(unittest.TestCase):
 
     def test_challenge8_api(self):
-        self.search_using_api("xbox")
+        search_tags = ["roku", "xbox", "iphone", "chrome"]
+        for tag in search_tags:
+            self.search_using_api(tag)
 
     @staticmethod
     def search_using_api(search_text="roku"):
@@ -21,14 +23,13 @@ class Challenge8(unittest.TestCase):
         resp = requests.get("https://help.sling.com/en/support/search.json?term=%s"%(search_text))
         print("resp.ok : {}".format(resp.ok))  # => True
         print("resp.status_code: {}".format(resp.status_code))  # => 200
-        file = open("response.txt", "w")
+        file = open("response_{}.txt".format(search_text), "w")
         file.write(resp.text)
         file.close()
         response_dict = json.loads(resp.text)
-        #Print search result titles
         for item in response_dict:
             print(item["title"])
-        print(colored("Total search result count: {}".format(len(response_dict)), 'green'))
+        print(colored("Total search result for {} count: {}".format(search_text, len(response_dict)), 'green'))
 
 if __name__ == '__main__':
     unittest.main()
