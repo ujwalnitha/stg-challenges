600a4395bcaef3fc23c76dc99a88adebf6fc865b
Nitha Ujwal <nitha.ujwal@dish.com>
2020-02-11 13:05:38 -0700

    Challenge 8

--- challenge8/challeng8_api.py
+++ challenge8/challeng8_api.py
@@ -1,34 +1,34 @@
 # api testing reference : https://chercher.tech/python/api-testing-selenium-python
 # http://www.compjour.org/tutorials/intro-to-python-requests-and-json/
-# This test is to search for different models and get total results count to a text file
+# This test is to sling help search and get total results count
+# Response will be written to a text file, response.txt at current location
 # Using API testing, not from UI
-import json
+
 import unittest
 import requests
+import json
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from termcolor import colored
 
 class Challenge8(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Chrome("../chromedriver")
-
-    def tearDown(self):
-        self.driver.close()
-
     def test_challenge8_api(self):
-        self.search_using_api("porsche")
+        self.search_using_api("xbox")
 
     @staticmethod
-    def search_using_api(self, search_text=""):
-        resp = requests.get("https://help.sling.com/en/support/search?term={}".format(search_text))
-        #resp = requests.get("https://www.copart.com/lotSearchResults/?free=true&query={}".format(search_text))
+    def search_using_api(search_text="roku"):
+        print("Search text : %s" %(search_text))
+        resp = requests.get("https://help.sling.com/en/support/search.json?term=%s"%(search_text))
         print("resp.ok : {}".format(resp.ok))  # => True
         print("resp.status_code: {}".format(resp.status_code))  # => 200
-        print("resp.headers['content-type'] : {}".format(resp.headers['content-type']))  # => "text/html"
-        print("resp.text : \n {}".format(resp.text))
-
+        file = open("response.txt", "w")
+        file.write(resp.text)
+        file.close()
+        response_dict = json.loads(resp.text)
+        #Print search result titles
+        for item in response_dict:
+            print(item["title"])
+        print(colored("Total search result count: {}".format(len(response_dict)), 'green'))
 
 if __name__ == '__main__':
     unittest.main()
